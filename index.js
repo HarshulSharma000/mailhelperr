dc152ff596eca6ad0273efad734c717c2125d81d
Harshul Sharma <harshulsharma000@outlook.com>
2017-10-04 23:54:42 +0530

    Passport google authentication added and rerouting to surveys built within

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 'use strict';
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const {clientID,clientSecret } = require('./config/keys');
+
 const app = express();
 
+passport.use(new GoogleStrategy({
+    clientID,
+    clientSecret,
+    callbackURL: '/auth/google/callback/'
+    }, accesstoken => {
+    console.log("here is our accessstoken:");
+    console.log(accesstoken);
+    }
+));
+
+app.get('/auth/google',passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback',passport.authenticate('google'), 
+(req,res) => res.redirect('/surveys')
+);
+
+app.get('/surveys',(req,res) => res.send({ 
+    Le: "Aunty aa gya..."
+}));
+
 app.get('/',(req,res) => {
-    res.send({ Hi: "Hope this works here right now :)"});
+    res.send({ Bolna:"Aunty aau kya?"});
 });
 
 const PORT = process.env.PORT || 5000;
