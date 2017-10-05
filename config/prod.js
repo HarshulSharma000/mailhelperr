1df645a65814bce91947f6fd075f5905e0438348
Harshul Sharma <harshulsharma000@outlook.com>
2017-10-05 23:13:56 +0530

    Keys files edited for using with both production and dev environment

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongodbURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
